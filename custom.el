6939c1dd1f6d2a2d13b84babe839257e41e25e92
BodeanTheZelous <benjamin.frazer.1997@gmail.com>
2021-09-23 21:50:23 +0100

    initial commital

---
+++ custom.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   '"c:/Users/b0628/.doom.d/orgfiles.text")
+ '(org-directory '"~/Nextcloud3/GuDocs/NoteBook"))
+
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
