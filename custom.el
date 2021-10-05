16fbefeba7c7a2bac6d621d04562e816982901e6
BodeanTheZelous <benjamin.frazer.1997@gmail.com>
2021-10-05 18:51:30 +0100

    general fiddling wit python IDE settings and themes + fonts

--- custom.el
+++ custom.el
@@ -3,9 +3,47 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files
-   '"c:/Users/b0628/.doom.d/orgfiles.text")
- '(org-directory '"~/Nextcloud3/GuDocs/NoteBook"))
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#3F3F3F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#DCDCDC"])
+ '(custom-safe-themes
+   '("92591f7527410be0666c6c65db3d4bf7346cf94595666a4ee0be356f4a5063b0" "51bff19f142e39bde517a1af0d0d1c3bb0ac208d11dbbf33fc26b91f68470317" "c3595dd3ec6ebc2651127d3452b357dfc48613a4806e0630f288ce62cb068f54" "e7ef6aa6e0c7a380d32bb3bc0394cf65d77c21dec06ab790adac90fc12f6191c" "0fbab0dec729e2a81617dcdff28ae0a30124f8764e43ff8a97d2d18f7cebd5af" "67b7eb85848d60cc80cb59cae1c27d6ec45661b316bed1dcfac4530f3e264f11" "6041a732c5f03d92ac11dc30dea2ec674d11b1340772387ae327041edceb36cb" "34235be96d680dee818d58371286aed3c13441f287156bb03f302c46637bcfca" "039b086d595ebc1e32a33039dafa2003569ede1abb3c0022cc35974887b4c58f" "09ab4c8bcd01a1275d0460bd8ae8296319c594b2f40c8c36a056cd2f53842977" "dfa89ce2fc12ce8f173e8982ca0d455e7524b513e6696c0953d2df1354a5195c" "05604fd187db30368023d71e9d96d4073ec4beeaa2050ca8699d2b50be7a4040" "5b17a7ce8defb9f191b9d1417223cc0a61b814971c385da938c2d6245b2fdcc3" "af77e2c5c43f9f00f9c983f07d25257ce62f96d333dfddeb813d863daa9093f4" "6ac168552a849fb997ca06a77c854f3a73247fbfe3d4530bcac731a6f496bc1d" "6e8e2c856ba00dc6ff7ec72ef409bbf9a72354430a4fe610c51179675ff64b96" "0a60c6265487ddcf3e4cf9c430a390abcbd7b39b998672fe3a47edf62a346050" "d97265d5f3587eb942f63d0f42e7ee07d281f93f698245e2680a35fd8940947e" "e8bbe184ba98a77000a75b261993c3aa4d24e0d1224a96784315b0dae1d6f64a" "55a1a4c438a0517d916959a8a177af489417fa1d0db76fe1149261ced31437f8" "860a2d0be28108334c92c07ad4293ad8b4138ef5d5af700e754974401a9ad753" default))
+ '(exwm-floating-border-color "#262626")
+ '(fci-rule-color "#4F4F4F")
+ '(highlight-tail-colors ((("#454845") . 0) (("#474f4f") . 20)))
+ '(ispell-program-name '"C://Program Files/hunspell/bin/hunspell.exe")
+ '(jdee-db-active-breakpoint-face-colors (cons "#000000" "#8CD0D3"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#000000" "#7F9F7F"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#000000" "#494949"))
+ '(objed-cursor-color "#CC9393")
+ '(org-agenda-files '"c:/Users/b0628/.doom.d/orgfiles.text")
+ '(org-directory '"~/Nextcloud3/GuDocs/NoteBook")
+ '(pdf-view-midnight-colors (cons "#DCDCDC" "#3F3F3F"))
+ '(rustic-ansi-faces
+   ["#3F3F3F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#DCDCDC"])
+ '(vc-annotate-background "#3F3F3F")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#7F9F7F")
+    (cons 40 "#a4b48f")
+    (cons 60 "#cac99f")
+    (cons 80 "#F0DFAF")
+    (cons 100 "#eacfa4")
+    (cons 120 "#e4bf99")
+    (cons 140 "#DFAF8F")
+    (cons 160 "#dea3a0")
+    (cons 180 "#dd97b1")
+    (cons 200 "#DC8CC3")
+    (cons 220 "#d68eb3")
+    (cons 240 "#d190a3")
+    (cons 260 "#CC9393")
+    (cons 280 "#ab8080")
+    (cons 300 "#8a6d6d")
+    (cons 320 "#695b5b")
+    (cons 340 "#4F4F4F")
+    (cons 360 "#4F4F4F")))
+ '(vc-annotate-very-old-color nil))
 
 
 (custom-set-faces
