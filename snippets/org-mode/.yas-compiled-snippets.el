eda1afad59f10ac6a0bcc2bffa92e35ec6c57940
BodeanTheZelous <benjamin.frazer.1997@gmail.com>
2021-11-14 21:23:09 +0000

    some more sippets

--- snippets/org-mode/.yas-compiled-snippets.el
+++ snippets/org-mode/.yas-compiled-snippets.el
@@ -2,11 +2,13 @@
 ;;; Snippet definitions:
 ;;;
 (yas-define-snippets 'org-mode
-                     '(("latexEquation_Org" "\\\\begin{align}\n$1\n\\\\end{align}\n" "latexEquation_Org" nil nil nil "c:/Users/b0628/.doom.d/snippets/org-mode/latexEquation_Org" nil nil)
+                     '(("python_source_basic" "#+begin_src python\n    $1\n#+end_src" "python_source_basic" nil nil nil "c:/Users/b0628/.doom.d/snippets/org-mode/python_source_basic" nil nil)
+                       ("latexEquation_Org" "\\\\begin{align}\n$1\n\\\\end{align}\n" "latexEquation_Org" nil nil nil "c:/Users/b0628/.doom.d/snippets/org-mode/latexEquation_Org" nil nil)
+                       ("case_Latex" "\\begin{align}\n$1=\n    \\begin{cases}\n    $2, & $3 \\\\\\\n    $4, & $5\n    \\end{cases}\n\\end{align}\n" "case_Latex" nil nil nil "c:/Users/b0628/.doom.d/snippets/org-mode/case_Latex" nil nil)
                        ("Lim_Latex" "\\lim\\limits_{$1 \\to $2}" "Lim_Latex" nil nil nil "c:/Users/b0628/.doom.d/snippets/org-mode/Lim_Latex" nil nil)
                        ("LaplaceTransform_Latex" "\\mathlarger{\\mathcal{L}}\\left\\\\{ $1 \\right\\\\}" "LaplaceTransform_Latex" nil nil nil "c:/Users/b0628/.doom.d/snippets/org-mode/LaplaceTransform_Latex" nil nil)
                        ("InverseLaplaceTransform_Latex" "\\mathlarger{\\mathcal{L}}^{-1}\\left\\\\{ $1 \\right\\\\}" "InverseLaplaceTransform_Latex" nil nil nil "c:/Users/b0628/.doom.d/snippets/org-mode/InverseLaplaceTransform_Latex" nil nil)
                        ("FancySymbol_Latex" "\\mathlarger{\\mathcal{$1}}" "FancySymbol_Latex" nil nil nil "c:/Users/b0628/.doom.d/snippets/org-mode/FancySymbol_Latex" nil nil)))
 
 
-;;; Do not edit! File generated at Thu Nov 11 23:26:18 2021
+;;; Do not edit! File generated at Sun Nov 14 14:57:48 2021
